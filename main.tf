5f6ed0611d2e53a39907bedfedfd79a800ae5c5f
rizi82 <smartybcs@gmail.com>
2019-07-21 22:07:55 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
-test
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+module "webserver_dev"{
+ source  = "./modules/webserver"
+ cluster_name = "webservers-dev"
+ instance_type = "t2.micro"
+  min_size = 2
+  max_size = 3
+}
